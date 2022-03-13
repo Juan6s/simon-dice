de17455b4a194c5617bd606c34a5fbd4936a3ae4
Juan Francisco Carrizo <francarrizo2001@gmail.com>
2022-03-13 09:49:15 -0300

    casi-terminado

--- js/accionar.js
+++ js/accionar.js
@@ -1,12 +1,33 @@
+const $botonRojo = document.querySelector("#boton-rojo");
+const $botonAmarillo = document.querySelector("#boton-amarillo");
+const $botonVerde = document.querySelector("#boton-verde");
+const $botonAzul = document.querySelector("#boton-azul");
+
 function accionarBotonRojo(){
     document.querySelector("#sonido-rojo").play();
+    $botonRojo.name = "activado";
+    setTimeout(apagarBoton,40, $botonRojo);
+
 }
 function accionarBotonAzul(){
     document.querySelector("#sonido-azul").play();
+    $botonAzul.name = "activado";
+    setTimeout(apagarBoton,40, $botonAzul);
 }
 function accionarBotonVerde(){
     document.querySelector("#sonido-verde").play();
+    $botonVerde.name = "activado";
+    setTimeout(apagarBoton,40, $botonVerde);
 }
 function accionarBotonAmarillo(){
     document.querySelector("#sonido-amarillo").play();
+    $botonAmarillo.name = "activado";
+    setTimeout(apagarBoton,40, $botonAmarillo);
+}
+
+
+function apagarBoton($boton){
+    
+
+    $boton.name=""; 
 }
--- js/main.js
+++ js/main.js
@@ -1,21 +1,143 @@
 
+let secuenciaUsuario = [];
+let secuenciaMaquina = [];
+let turnoUsuario = false;
 
 
-
-document.querySelector("#boton-rojo").onclick = function(event){
-    event.preventDefault();
-    accionarBotonRojo();
+function agregarSecuencia() {
+    secuenciaMaquina.push(random(1, 4));
 }
-document.querySelector("#boton-amarillo").onclick = function(event){
+
+function reproducirSecuencia(indice) {
+
+    switch (secuenciaMaquina[indice]) {
+
+        case 1:
+            accionarBotonAmarillo();
+
+            break;
+        case 2:
+            accionarBotonAzul();
+
+            break;
+        case 3:
+            accionarBotonRojo();
+
+            break;
+        case 4:
+            accionarBotonVerde();
+            break;
+        default:
+            return;
+
+
+    }
+
+
+}
+
+function turnoMaquina() {
+    agregarSecuencia();
+    document.querySelector("#contador").innerText = secuenciaMaquina.length;
+
+    let i = 0;
+    setInterval(function () {
+        if (i === secuenciaMaquina.length) {
+            turnoUsuario = true;
+            clearInterval();
+        }
+        reproducirSecuencia(i);
+        i++;
+    }, 700);
+}
+
+function iniciarJuego() {
+    secuenciaMaquina = [];
+    secuenciaUsuario = [];
+    turnoUsuario = false;
+    turnoMaquina();
+}
+
+
+function compararSecuencias() {
+
+    if (secuenciaUsuario.length === secuenciaMaquina.length) {
+        for(let i = 0; i<secuenciaMaquina.length; i++){
+            if(secuenciaMaquina[i] !== secuenciaUsuario[i]){
+                secuenciaUsuario = [];
+                return false;
+            }
+        }
+        secuenciaUsuario=[]
+        return true;    
+        
+    }
+
+}
+
+
+
+document.querySelector(".panel-botones").onclick = function (event) {
     event.preventDefault();
+
+    resultadoComparacion = compararSecuencias();
+
+    if (resultadoComparacion === false) {
+        document.querySelector("#contador").innerText = "-";
+    } else if (resultadoComparacion === true) {
+        setTimeout(turnoMaquina, 700);
+    }
+}
+
+document.querySelector("#boton-comenzar").onclick = function (event) {
+    event.preventDefault();
+    iniciarJuego();
+
+
+}
+
+
+document.querySelector("#boton-amarillo").onclick = function (event) {
+    event.preventDefault();
+    if (turnoUsuario) {
+        accionarBotonAmarillo();
+        secuenciaUsuario.push(1)
+    }
     accionarBotonAmarillo();
 }
-document.querySelector("#boton-azul").onclick = function(event){
+document.querySelector("#boton-azul").onclick = function (event) {
     event.preventDefault();
-    accionarBotonAzul();
+    if (turnoUsuario) {
+        accionarBotonAzul()
+        secuenciaUsuario.push(2);
+    }
+
+
 }
-document.querySelector("#boton-verde").onclick = function(event){
+
+document.querySelector("#boton-rojo").onclick = function (event) {
     event.preventDefault();
-    alert("Sadasd")
-    accionarBotonVerde();
+    if (turnoUsuario) {
+        accionarBotonRojo();
+        secuenciaUsuario.push(3)
+    }
+
+}
+
+document.querySelector("#boton-verde").onclick = function (event) {
+    event.preventDefault();
+    if (turnoUsuario) {
+        accionarBotonVerde();
+        secuenciaUsuario.push(4)
+    }
+
+}
+
+
+
+
+
+function random(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+
 }
